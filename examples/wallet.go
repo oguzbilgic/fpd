ee49c479a0b13f0214d176a2640f5bac2881f634
Oguz Bilgic <fisyonet@gmail.com>
2013-10-12 11:55:35 +0000

    Add examples/wallet.go

---
+++ examples/wallet.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/oguzbilgic/fpd"
+	"fmt"
+)
+
+func main() {
+	// Buy price of the security: $136.02
+	buyPrice := fpd.New(13602000, -5)
+
+	// Sell price of the security: $137.699
+	sellPrice := fpd.New(13769900, -5)
+
+	// Volume traded: 0.01
+	volume := fpd.New(1000000, -8)
+
+	// Trade fee percentage: 0.6%
+	feePer := fpd.New(6, -3)
+
+	buyCost := buyPrice.Mul(volume)
+	buyFee := buyPrice.Mul(volume).Mul(feePer)
+	sellRevenue := sellPrice.Mul(volume)
+	sellFee := sellPrice.Mul(volume).Mul(feePer)
+
+	// Initall account balance: $2.00000
+	balance := fpd.New(200000, -5)
+
+	balance = balance.Sub(buyCost)
+	balance = balance.Sub(buyFee)
+	balance = balance.Add(sellRevenue)
+	balance = balance.Sub(sellFee)
+
+	// Final balance
+	fmt.Println(balance)
+	// Did this trade turn into profit? :)
+}
